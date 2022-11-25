a8a8047d9bcbfd9e9746b8957df854b377c671bb
nilarcs <nilarcs@hotmail.com>
2022-11-25 23:19:54 +0800

    modi webpack prod config

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -1,49 +1,49 @@
-var webpack = require('webpack');
-var path = require('path');
-var rules = require('./webpack.rules');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
-var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+var webpack = require("webpack");
+var path = require("path");
+var rules = require("./webpack.rules");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin");
+var WebpackCleanupPlugin = require("webpack-cleanup-plugin");
+var BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 var artifacts = require("../test/artifacts");
 
 var OUTPATH = artifacts.pathSync("/build");
 
 module.exports = {
+  mode: "production",
   entry: {
-    app: './src/index.jsx',
+    app: "./src/index.jsx",
   },
   output: {
     path: OUTPATH,
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[contenthash].js'
+    filename: "[name].[hash].js",
+    chunkFilename: "[contenthash].js",
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
   },
   module: {
-    noParse: [
-      /mapbox-gl\/dist\/mapbox-gl.js/
-    ],
-    rules: rules
+    noParse: [/mapbox-gl\/dist\/mapbox-gl.js/],
+    rules: rules,
   },
   node: {
     fs: "empty",
-    net: 'empty',
-    tls: 'empty'
+    net: "empty",
+    tls: "empty",
   },
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
     new WebpackCleanupPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
+      "process.env": {
+        NODE_ENV: '"production"',
+      },
     }),
     new HtmlWebpackPlugin({
-      template: './src/template.html',
-      title: 'Maputnik'
+      template: "./src/template.html",
+      title: "Maputnik",
     }),
     new HtmlWebpackInlineSVGPlugin({
       runPreEmit: true,
@@ -51,18 +51,18 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: './src/manifest.json',
-          to: 'manifest.json'
-        }
-      ]
+          from: "./src/manifest.json",
+          to: "manifest.json",
+        },
+      ],
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      defaultSizes: 'gzip',
+      analyzerMode: "static",
+      defaultSizes: "gzip",
       openAnalyzer: false,
       generateStatsFile: true,
-      reportFilename: 'bundle-stats.html',
-      statsFilename: 'bundle-stats.json',
-    })
-  ]
+      reportFilename: "bundle-stats.html",
+      statsFilename: "bundle-stats.json",
+    }),
+  ],
 };
--- src/components/ModalExport.jsx
+++ src/components/ModalExport.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Slugify from 'slugify'
-import { saveAs } from 'file-saver'
-import pkgLockJson from '../../package-lock.json'
+import React from "react";
+import PropTypes from "prop-types";
+import Slugify from "slugify";
+import { saveAs } from "file-saver";
+import pkgLockJson from "../../package-lock.json";
 
-import {format} from '@mapbox/mapbox-gl-style-spec'
-import FieldString from './FieldString'
-import FieldCheckbox from './FieldCheckbox'
-import InputButton from './InputButton'
-import Modal from './Modal'
-import {MdFileDownload} from 'react-icons/md'
-import style from '../libs/style'
-import fieldSpecAdditional from '../libs/field-spec-additional'
+import { format } from "@mapbox/mapbox-gl-style-spec";
+import FieldString from "./FieldString";
+import FieldCheckbox from "./FieldCheckbox";
+import InputButton from "./InputButton";
+import Modal from "./Modal";
+import { MdFileDownload } from "react-icons/md";
+import style from "../libs/style";
+import fieldSpecAdditional from "../libs/field-spec-additional";
 
-
+// const MAPBOX_GL_VERSION = "1.13.2"; //pkgLockJson.dependencies["mapbox-gl"].version;
 const MAPBOX_GL_VERSION = pkgLockJson.dependencies["mapbox-gl"].version;
-
 
 export default class ModalExport extends React.Component {
   static propTypes = {
@@ -23,29 +22,27 @@ export default class ModalExport extends React.Component {
     onStyleChanged: PropTypes.func.isRequired,
     isOpen: PropTypes.bool.isRequired,
     onOpenToggle: PropTypes.func.isRequired,
-  }
+  };
 
   constructor(props) {
     super(props);
   }
 
-  tokenizedStyle () {
+  tokenizedStyle() {
     return format(
-      style.stripAccessTokens(
-        style.replaceAccessTokens(this.props.mapStyle)
-      )
+      style.stripAccessTokens(style.replaceAccessTokens(this.props.mapStyle))
     );
   }
 
-  exportName () {
-    if(this.props.mapStyle.name) {
+  exportName() {
+    if (this.props.mapStyle.name) {
       return Slugify(this.props.mapStyle.name, {
-        replacement: '_',
+        replacement: "_",
         remove: /[*\-+~.()'"!:]/g,
-        lower: true
+        lower: true,
       });
     } else {
-      return this.props.mapStyle.id
+      return this.props.mapStyle.id;
     }
   }
 
@@ -79,14 +76,16 @@ export default class ModalExport extends React.Component {
 </html>
 `;
 
-    const blob = new Blob([html], {type: "text/html;charset=utf-8"});
+    const blob = new Blob([html], { type: "text/html;charset=utf-8" });
     const exportName = this.exportName();
     saveAs(blob, exportName + ".html");
   }
 
   downloadStyle() {
     const tokenStyle = this.tokenizedStyle();
-    const blob = new Blob([tokenStyle], {type: "application/json;charset=utf-8"});
+    const blob = new Blob([tokenStyle], {
+      type: "application/json;charset=utf-8",
+    });
     const exportName = this.exportName();
     saveAs(blob, exportName + ".json");
   }
@@ -96,67 +95,84 @@ export default class ModalExport extends React.Component {
       ...this.props.mapStyle,
       metadata: {
         ...this.props.mapStyle.metadata,
-        [property]: value
-      }
-    }
-    this.props.onStyleChanged(changedStyle)
+        [property]: value,
+      },
+    };
+    this.props.onStyleChanged(changedStyle);
   }
-
 
   render() {
-    return <Modal
-      data-wd-key="modal:export"
-      isOpen={this.props.isOpen}
-      onOpenToggle={this.props.onOpenToggle}
-      title={'Export Style'}
-      className="maputnik-export-modal"
-    >
+    return (
+      <Modal
+        data-wd-key="modal:export"
+        isOpen={this.props.isOpen}
+        onOpenToggle={this.props.onOpenToggle}
+        title={"Export Style"}
+        className="maputnik-export-modal"
+      >
+        <section className="maputnik-modal-section">
+          <h1>Download Style</h1>
+          <p>Download a JSON style to your computer.</p>
 
-      <section className="maputnik-modal-section">
-        <h1>Download Style</h1>
-        <p>
-          Download a JSON style to your computer.
-        </p>
+          <div>
+            <FieldString
+              label={fieldSpecAdditional.maputnik.mapbox_access_token.label}
+              fieldSpec={fieldSpecAdditional.maputnik.mapbox_access_token}
+              value={
+                (this.props.mapStyle.metadata || {})[
+                  "maputnik:mapbox_access_token"
+                ]
+              }
+              onChange={this.changeMetadataProperty.bind(
+                this,
+                "maputnik:mapbox_access_token"
+              )}
+            />
+            <FieldString
+              label={fieldSpecAdditional.maputnik.maptiler_access_token.label}
+              fieldSpec={fieldSpecAdditional.maputnik.maptiler_access_token}
+              value={
+                (this.props.mapStyle.metadata || {})[
+                  "maputnik:openmaptiles_access_token"
+                ]
+              }
+              onChange={this.changeMetadataProperty.bind(
+                this,
+                "maputnik:openmaptiles_access_token"
+              )}
+            />
+            <FieldString
+              label={
+                fieldSpecAdditional.maputnik.thunderforest_access_token.label
+              }
+              fieldSpec={
+                fieldSpecAdditional.maputnik.thunderforest_access_token
+              }
+              value={
+                (this.props.mapStyle.metadata || {})[
+                  "maputnik:thunderforest_access_token"
+                ]
+              }
+              onChange={this.changeMetadataProperty.bind(
+                this,
+                "maputnik:thunderforest_access_token"
+              )}
+            />
+          </div>
 
-        <div>
-          <FieldString
-            label={fieldSpecAdditional.maputnik.mapbox_access_token.label}
-            fieldSpec={fieldSpecAdditional.maputnik.mapbox_access_token}
-            value={(this.props.mapStyle.metadata || {})['maputnik:mapbox_access_token']}
-            onChange={this.changeMetadataProperty.bind(this, "maputnik:mapbox_access_token")}
-          />
-          <FieldString
-            label={fieldSpecAdditional.maputnik.maptiler_access_token.label}
-            fieldSpec={fieldSpecAdditional.maputnik.maptiler_access_token}
-            value={(this.props.mapStyle.metadata || {})['maputnik:openmaptiles_access_token']}
-            onChange={this.changeMetadataProperty.bind(this, "maputnik:openmaptiles_access_token")}
-          />
-          <FieldString
-            label={fieldSpecAdditional.maputnik.thunderforest_access_token.label}
-            fieldSpec={fieldSpecAdditional.maputnik.thunderforest_access_token}
-            value={(this.props.mapStyle.metadata || {})['maputnik:thunderforest_access_token']}
-            onChange={this.changeMetadataProperty.bind(this, "maputnik:thunderforest_access_token")}
-          />
-        </div>
+          <div className="maputnik-modal-export-buttons">
+            <InputButton onClick={this.downloadStyle.bind(this)}>
+              <MdFileDownload />
+              Download Style
+            </InputButton>
 
-        <div className="maputnik-modal-export-buttons">
-          <InputButton
-            onClick={this.downloadStyle.bind(this)}
-          >
-            <MdFileDownload />
-            Download Style
-          </InputButton>
-
-          <InputButton
-            onClick={this.downloadHtml.bind(this)}
-          >
-            <MdFileDownload />
-            Download HTML
-          </InputButton>
-        </div>
-      </section>
-
-    </Modal>
+            <InputButton onClick={this.downloadHtml.bind(this)}>
+              <MdFileDownload />
+              Download HTML
+            </InputButton>
+          </div>
+        </section>
+      </Modal>
+    );
   }
 }
-
